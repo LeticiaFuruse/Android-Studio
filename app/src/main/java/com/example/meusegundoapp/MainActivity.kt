03472c8a9082e33f55a83ce61640d35bbd984160
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-07 04:29:30 -0300

    criando tela play e adicionando novas imagens

--- app/src/main/java/com/example/meusegundoapp/MainActivity.kt
+++ app/src/main/java/com/example/meusegundoapp/MainActivity.kt
@@ -68,4 +68,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
 }
--- app/src/main/java/com/example/meusegundoapp/MenuSpotfy.kt
+++ app/src/main/java/com/example/meusegundoapp/MenuSpotfy.kt
@@ -1,6 +1,8 @@
 package com.example.meusegundoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,12 @@ import androidx.core.view.WindowInsetsCompat
 class MenuSpotfy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu_spotfy)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+    }
+
+    fun btnPlayMusic(view: View){
+        var intent = Intent(this,play_spotfy()::class.java)
+        startActivity(intent)
     }
 }
